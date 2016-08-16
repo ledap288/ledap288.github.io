1b8dbbeee03609a0a2fd115c9b74d5bbc3f7c038
Pavel <ledap288@gmail.com>
2016-08-16 03:44:46 +0300

    highedge v.0.1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+
+    $('.parallax').parallax();
+});
