ce22d92f81f60d93b9f476b055196d552639f809
Pavel Zabaikin <ledap288@gmail.com>
2018-05-12 13:52:44 +0200

    simple portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new TypeIt(document.querySelector('.job-title'), {
+    strings: ["Frontend Devel", "Software Enginee", "Ah, Whatever Developer."],
+    speed: 100,
+    breakLines: false,
+    autoStart: false
+});
