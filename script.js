3e0d8dc3fc6c6a18aa4565c4a0ab9f80cff37f3b
Pavel Zabaikin <ledap288@gmail.com>
2019-02-01 10:52:18 +0100

    info updated

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var typed = new TypeIt(document.querySelector('.job-title'), {
-    strings: ["Frontend Devel", "Software Enginee", "Ah, Whatever Developer."],
-    speed: 100,
+var elem = document.querySelector('.job-title');
+var typed = new TypeIt(elem, {
+    strings: ["Frontend Devel", "Software Enginee", "Ah, Entrepreneur and Whatever Developer.", " \n Currently - Co-Founder & Lead Engineer at Jarvify  "],
+    speed: 50,
     breakLines: false,
-    autoStart: false
+    autoStart: false,
+    onComplete: function() {
+        console.log('fuck');
+        document.querySelector('.ti-cursor').remove();
+    }
 });
